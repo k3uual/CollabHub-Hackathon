74596ac2eb808159d55d36b1c97c686bbcab1e4e
k3uual <kewaltailor345@gmail.com>
2024-02-21 15:05:41 +0530

    Event registration HTML

--- Register.php
+++ Register.php
@@ -2,13 +2,7 @@
     <head>
         <title>Register</title>
     </head>
-    <style>
-        input[type="number"]::-webkit-inner-spin-button,
-        input[type="number"]::-webkit-outer-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-        }
-    </style>
+    
     <body>
         <h1>Register:</h1>
         <form action="Login.html" method="post">
@@ -31,7 +25,7 @@
             <input type="email" name="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$">
 
             <label for="mobno">Mobile No.:</label>
-            <input type="number" name="mobno" required>
+            <input type="tel" name="mobno" required>
 
             <label for="pass">Password: </label>
             <input type="password" id="typepass" name="pass" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
